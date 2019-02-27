ca482731c6d6bf12333aeb1c3d402075c8fbad0d
Rebecca Burton <Rebecca.Burton@ghyston.com>
2019-02-27 20:01:12 +0000

    Added template and updated to parse text file in template format

--- wikify.py
+++ wikify.py
@@ -1,22 +1,11 @@
 from pathlib import Path
+import os
 
+filepath = os.getcwd()
 
-wikiRoot = Path("C:/Users/user/Documents/Other/Lit/Wikis")
-
-# print(wikiRoot)
-# wikiToUse = Path / "TME Wiki/Pages"
-# fileToOpen = wikiToUse / "Helvina"
-
-filepath = "C:/Users/user/Documents/Other/Lit/Wikis/TME Wiki/Pages/"
-
-pageTitle = "Helvina"
-pageTitle = "Eldanan Empire"
-
-f = open(filepath + pageTitle + ".txt")
-
-links =	{
+links = {
   "The Millennium Empire_novel": "The Millennium Empire",
-  "The Millennium Empire_novel": "The Millennium Empire",
+  "The Millennium Empire_place": "Eldanan Empire",
   "Helvina": "Helvina",
   "Farida": "Farida",
   "Hegedar": "Hegedar",
@@ -25,44 +14,82 @@ links =	{
 }
 
 def makeLink(tag):
-	return "<a href=\"%s.html\">%s</a>" % (links.get(tag, tag), tag if tag.find("_") == -1 else tag[:tag.find("_")])
-
-h = f.read()
+    return "<a href=\"%s.html\">%s</a>" % (links.get(tag, tag), tag if tag.find("_") == -1 else tag[:tag.find("_")])
 
 def convertLinks(page):
-	convertedPage = ""
-	pageLines = page.split("\n")
-	for line in pageLines:
-		tagStart = 0
-		convertedLine = ""
-		lastMark = 0
-		while tagStart != -1:
-			tagStart = line.find("[", lastMark)
-			tagEnd = line.find("]", tagStart)
-			if tagStart == -1:
-				if lastMark == 0:
-					convertedLine = line
-				else:
-					convertedLine = convertedLine + line[lastMark+1:]
-			else:
-				if lastMark == 0:
-					convertedLine = convertedLine + line[:tagStart] + makeLink(line[tagStart+1:tagEnd])
-				else:
-					convertedLine = convertedLine + line[lastMark+1:tagStart] + makeLink(line[tagStart+1:tagEnd])
-			lastMark = tagEnd
-		convertedPage = convertedPage + convertedLine + "\n"
-	return convertedPage
+    convertedPage = ""
+    pageLines = page.split("\n")
+    for line in pageLines:
+        tagStart = 0
+        convertedLine = ""
+        lastMark = 0
+        while tagStart != -1:
+            tagStart = line.find("<", lastMark)
+            tagEnd = line.find(">", tagStart)
+            if tagStart == -1:
+                if lastMark == 0:
+                    convertedLine = line
+                else:
+                    convertedLine = convertedLine + line[lastMark+1:]
+            else:
+                if lastMark == 0:
+                    convertedLine = convertedLine + line[:tagStart] + makeLink(line[tagStart+1:tagEnd])
+                else:
+                    convertedLine = convertedLine + line[lastMark+1:tagStart] + makeLink(line[tagStart+1:tagEnd])
+            lastMark = tagEnd
+        convertedPage = convertedPage + convertedLine + "\n"
+    return convertedPage
 
-g = open(filepath + pageTitle + ".html", "w")
+def splitPage(page):
+    return page.split("########")
 
+def parseHeader(header):
+    headerData = {}
+    headerLines = header.split("\n")
+    for line in headerLines:
+        if (line.find("=") != -1):
+            parameterAndValue = line.split("=")
+            parameter = parameterAndValue[0]
+            value = parameterAndValue[1]
+            if (value.find("[") != -1):
+                valueList = value[1:-1].split(",")
+            if parameter == "Title":
+                headerData["Title"] = value
+            elif parameter == "Tags":
+                headerData["Tags"] = valueList
+            elif parameter == "Categories":
+                headerData["Categories"] = valueList
+    return headerData
 
-g.write("<!DOCTYPE html>")
-g.write("\n<html>")
-g.write("\n<head>")
-g.write("\n</head>")
-g.write("\n<body>")
-g.write("\n" + convertLinks(h))
-g.write("\n</body>")
-g.write("\n</html>")
+def parseBody(body):
+    return convertLinks(body)
+
+def parsePage(page):
+    htmlFile = "<!DOCTYPE html>\n<html>\n<head>\n<title>"
+    headerBody = splitPage(page)
+    header = headerBody[0]
+    body = headerBody[1]
+    headerData = parseHeader(header)
+    htmlFile += headerData.get("Title", "")
+    htmlFile += "</title>\n</head>\n<body>\n"
+    htmlFile += parseBody(body)
+    htmlFile += "\n</body>\n</html>"
+    return htmlFile
+
+linksPage = open(filepath + "/links.txt", "w")
+
+pageTitles = [
+    "Helvina",
+    "Eldanan Empire"
+]
+
+for pageTitle in pageTitles:
+    f = open(filepath + "/" + pageTitle + ".txt")
+
+    textPage = f.read()
+
+    wikiPage = open(filepath + "/" + pageTitle + ".html", "w")
+    htmlPage = parsePage(textPage)
+    wikiPage.write(htmlPage)
 
 
